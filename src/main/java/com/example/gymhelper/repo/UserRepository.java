8df808f93ac3da77af604652fc57a7fe83b0beb1
Bohdan Vasenshev <b.vasenshev61@gmail.com>
2023-04-05 12:34:23 +0200

    Initial commit

--- src/main/java/com/example/gymhelper/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gymhelper.repo;
-
-
-import com.example.gymhelper.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Person, Long> {
-}
